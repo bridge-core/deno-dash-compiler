d35f5d65979022b4c367c1e97a292615bc009be0
Joelant05 <64587014+Joelant05@users.noreply.github.com>
2022-07-10 14:55:25 +0100

    upd: automatically set network loopback exemption

--- src/WebSocket.ts
+++ src/WebSocket.ts
@@ -6,6 +6,8 @@ export class WebSocketServer {
     }
 
     start(port: number) {
+        this.setLoopbackExemption()
+
         const listener = Deno.listen({ port })
         listener.accept().then(async (connection) => {
             const httpConnection = Deno.serveHttp(connection)
@@ -33,5 +35,25 @@ export class WebSocketServer {
             },
         })
         if (this.socket && this.isStarted) this.socket.send(data)
+    }
+
+    async setLoopbackExemption() {
+        // Minecraft Retail Build
+        const retail = Deno.run({
+            cmd: ['CheckNetIsolation.exe', 'LoopbackExempt', '-a', '-p=S-1-15-2-1958404141-86561845-1752920682-3514627264-368642714-62675701-733520436'],
+            stdout: 'piped',
+        })
+        // Minecraft Preview Build
+        const preview = Deno.run({
+            cmd: ['CheckNetIsolation.exe', 'LoopbackExempt', '-a', '-p=S-1-15-2-424268864-5579737-879501358-346833251-474568803-887069379-4040235476'],
+            stdout: 'piped',
+        })
+
+        const isSuccess = (out: string) => out === 'OK.'
+        const decoder = new TextDecoder()
+        const retailOutput = decoder.decode(await retail.output()).trim()
+        const previewOutput = decoder.decode(await preview.output()).trim()
+        if (!isSuccess(retailOutput)) console.log('Unable to set network loopback exemption for Minecraft retail build.')
+        if (!isSuccess(previewOutput)) console.log('Unable to set network loopback exemption for Minecraft preview build.')
     }
 }
