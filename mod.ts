d6e2e10c32fa11bb45e1cd416b9234a46ea7300a
Outer Cloud Studio <outercloudstudio@gmail.com>
2025-01-29 03:07:50 -0600

    Bump to 1.1.0 and add message for using dev version

--- mod.ts
+++ mod.ts
@@ -6,42 +6,44 @@ import { comMojangFolder } from './src/comMojangFolder.ts'
 import { initRuntimes, swcVersion } from './src/deps.ts'
 
 type YargsInstance = ReturnType<typeof yargs>
-const CURRENT_VERSION = `1.0.0`
+const CURRENT_VERSION = `1.1.0`
 
 async function fetchLatestVersion(): Promise<string | null> {
-    try {
-        const response = await fetch('https://api.github.com/repos/bridge-core/deno-dash-compiler/releases/latest');
-        if (!response.ok) {
-            return null;
-        }
-        const data = await response.json();
-        return data.tag_name;
-    } catch (error) {
-        console.error('Error fetching the latest version:', error);
-        return null;
-    }
+	try {
+		const response = await fetch('https://api.github.com/repos/bridge-core/deno-dash-compiler/releases/latest')
+		if (!response.ok) {
+			return null
+		}
+		const data = await response.json()
+		return data.tag_name
+	} catch (error) {
+		console.error('Error fetching the latest version:', error)
+		return null
+	}
 }
 
-function compareVersions(current: string, latest: string): boolean {
-	const currentVersion = semverParse(current);
-	const latestVersion = semverParse(latest);
+function compareVersions(current: string, latest: string): number {
+	const currentVersion = semverParse(current)
+	const latestVersion = semverParse(latest)
 	if (!currentVersion || !latestVersion) {
-		throw new Error('Invalid version format');
+		throw new Error('Invalid version format')
 	}
-	return semverCompare(currentVersion, latestVersion) < 0;
+	return semverCompare(currentVersion, latestVersion)
 }
 
 async function checkForUpdates() {
-    const latestVersion = await fetchLatestVersion();
-    if (latestVersion && compareVersions(CURRENT_VERSION, latestVersion)) {
-        console.log(`%cA new version (${latestVersion}) is available. You are currently using version v${CURRENT_VERSION}.`, 'color: red; font-weight:bold;');
-    }
+	const latestVersion = await fetchLatestVersion()
+	if (latestVersion && compareVersions(CURRENT_VERSION, latestVersion) < 0) {
+		console.log(`%cA new version (${latestVersion}) is available. You are currently using version v${CURRENT_VERSION}.`, 'color: red; font-weight:bold;')
+	} else if (latestVersion && compareVersions(CURRENT_VERSION, latestVersion) > 0) {
+		console.log(`%cYou are using a dev version ${CURRENT_VERSION} compared to the latest version (${latestVersion}).`, 'color: green; font-weight:bold;')
+	}
 }
 
 initRuntimes(`https://esm.sh/@swc/wasm-web@${swcVersion}/wasm-web_bg.wasm`)
 
 if (import.meta.main) {
-	await checkForUpdates();
+	await checkForUpdates()
 	const cli = new CLI()
 
 	yargs(Deno.args)
@@ -98,8 +100,7 @@ if (import.meta.main) {
 						})
 						.option('reload', {
 							alias: 'r',
-							description:
-								'Quick reload for functions and scripts',
+							description: 'Quick reload for functions and scripts',
 							type: 'number',
 						})
 						// Need to use coerce rather than "default" so we can differentiate between when the option isn't used or is used without an argument
