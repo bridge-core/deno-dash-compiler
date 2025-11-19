a70f776d29599ae1350e6e0e05f49a116ccb082a
Thomas Orsbourne <thomas@gamemodeone.com>
2025-11-18 20:02:58 -0400

    Swap to GDK paths for building

--- src/comMojangFolder.ts
+++ src/comMojangFolder.ts
@@ -1,15 +1,12 @@
 import { path } from './deps.ts'
 
-const appDataFolder = Deno.env.get('LOCALAPPDATA')
+const appDataFolder = Deno.env.get("APPDATA");
 export const comMojangFolder = appDataFolder
-	? path.join(
-			appDataFolder,
-			'Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang'
-	  )
-	: null
+  ? path.join(appDataFolder, "Minecraft Bedrock/Users/Shared/games/com.mojang")
+  : null;
 export const previewComMojangFolder = appDataFolder
-	? path.join(
-			appDataFolder,
-			'Packages/Microsoft.MinecraftWindowsBeta_8wekyb3d8bbwe/LocalState/games/com.mojang'
-	  )
-	: null
+  ? path.join(
+      appDataFolder,
+      "Minecraft Bedrock Preview/Users/Shared/games/com.mojang"
+    )
+  : null;
