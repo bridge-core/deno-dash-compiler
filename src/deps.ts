42934e7a66a705b273d0ac9cab1c3ada039e1b62
Thomas Orsbourne <thomas@gamemodeone.com>
2026-06-03 11:55:30 -0300

    chore(deps): Bump to dash_compiler 0.13.0

--- src/deps.ts
+++ src/deps.ts
@@ -6,16 +6,16 @@ export {
   ProjectConfig,
 } from "https://esm.sh/@bridge-editor/mc-project-core@0.5.0";
 export { isMatch } from "npm:@bridge-editor/common-utils";
-// @deno-types="https://esm.sh/@bridge-editor/dash-compiler@0.12.0"
+// @deno-types="https://esm.sh/@bridge-editor/dash-compiler@0.13.0"
 export {
   Dash,
   FileSystem,
   initRuntimes,
-} from "https://unpkg.com/@bridge-editor/dash-compiler@0.12.0/dist/dash-compiler.bundled.es.js";
+} from "https://unpkg.com/@bridge-editor/dash-compiler@0.13.0/dist/dash-compiler.bundled.es.js";
 // @ts-ignore - json5 has a default export at runtime via esm.sh
 export { default as json5 } from "https://esm.sh/json5@2.2.3";
 export { debounce } from "jsr:@std/async";
-import { default as dashPackageJson } from "https://unpkg.com/@bridge-editor/dash-compiler@0.12.0/package.json" with { type: "json" };
+import { default as dashPackageJson } from "https://unpkg.com/@bridge-editor/dash-compiler@0.13.0/package.json" with { type: "json" };
 
 let swcVersion = dashPackageJson.dependencies["@swc/wasm-web"];
 if (swcVersion.startsWith("^")) swcVersion = swcVersion.slice(1);
